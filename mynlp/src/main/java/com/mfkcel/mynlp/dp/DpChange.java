1ee51aa021cceae0ca2d9acc97d3f79565100553
mfkcel <515546379@qq.com>
2019-11-06 19:26:05 +0800

    2019.11.6 company

--- mynlp/src/main/java/com/mfkcel/mynlp/dp/DpChange.java
+++ mynlp/src/main/java/com/mfkcel/mynlp/dp/DpChange.java
@@ -1,5 +1,8 @@
 package com.mfkcel.mynlp.dp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author mc1381288@163.com
  * @date 2019/11/2 11:18
@@ -11,13 +14,44 @@ package com.mfkcel.mynlp.dp;
  * 在学习算法时要注意，分析问题的思想过程
  *
  * 假设找零为x, 那么最终的结果为f(x) = x1 + x2 + x3 + ... + xn
+ *
+ * 假设含有的零钱类别为, changes[c1, c2, c3, c4, c5]
+ *
+ * 那么对应的最优解为:
+ *     f(c1) = 1, f(c2) = 1, f(c3) = 1, f(c4) = 1, f(c5) = 1, f(0.5) = 1, f(0.1) = 1, f(0) = 0(设0这个数是为了使逻辑连续)
+ *
+ * 状态转换为
+ *      min{f(cn)}
+ *
+ *  f(99) = f(99 - 50) + 1
+ *        = f(44 - 20) + 1
+ *        = f(22 - 20) + 1
+ *        = f(2 - 1) + 1
+ *        = f(1)
+ *
  */
 public class DpChange {
-    public static void main(String[] args) {
+    private static double[] changes = {100, 50, 20, 10, 5, 1, 0.5, 0.1};
 
+    public static void main(String[] args) {
+        Map<Double, Integer> doubleIntegerMap = solveDp(199.5);
+        System.out.println(doubleIntegerMap);
     }
 
-    public static double[] solveDp(double change) {
-        return null;
+    // 这个只计算了一种最优方案，不能计算找零的所有组合
+    public static Map<Double, Integer> solveDp(double change) {
+        Map<Double, Integer> resultMap = new HashMap<Double, Integer>();
+        for(int i = 0; i < changes.length; i++) {
+            int count = 0;
+            while(change - changes[i] >= 0) {
+                change = change - changes[i];
+                count++;
+            }
+
+            if(count == 0) continue;
+            resultMap.put(changes[i], count);
+        }
+
+        return resultMap;
     }
 }
--- mynlp/src/main/java/com/mfkcel/mynlp/wordsegmention/WordSegmentUnigram.java
+++ mynlp/src/main/java/com/mfkcel/mynlp/wordsegmention/WordSegmentUnigram.java
@@ -6,6 +6,19 @@ import java.util.Map;
  * create by mfkcel on 2019/11/2 22:40
  *
  * 使用枚举来进行分词产生所有可能的结果，然后使用语言模型来选择最好的分词结果(考虑词语上下文间的语义)
+ * 思路如下：
+ *         wordset  // 分词集合
+ *         worddict // 词典
+ *         sentence // 待分词句子
+ *         for i in 1 to n   //n 为词典中最长的词语
+ *              start = 0
+ *              for j in i to sentence_len - 1
+ *                   word = sentence.substring(start, j)
+ *                   if word not in wordset && word in worddict
+ *                      wordset.add(word)
+ *                      start = j + 1
+ *
+ *
  *
  * 使用数学公式表达就是:P(x1x2x3...xn)=P(x1)*P(x2)*P(x3)...P(xn)
  *
@@ -15,13 +28,6 @@ import java.util.Map;
  * 分词首先要有基本词典
  * 这个是通过统计可以计算出每个词组的频率--基本词典,一般是通过人工统计
  *
- * 我爱我的祖国
- *    我，爱，我，的，祖，国
- *    我，爱，我，的，祖国
- *    我，爱，我的，祖国
- *    我，爱我，的，祖国
- *    我爱我，的，祖国
- *    我爱我的，祖国
  */
 public class WordSegmentUnigram {
     public static Map<String, String[]> enumSegment(String content) {
