bb0737ef6cb638204eadcd0aaa0d6a37960ff058
zhouyong <515546379@qq.com>
2019-11-09 07:01:42 +0800

    2019.11.9 home

--- mynlp/src/main/java/com/mfkcel/mynlp/util/string/kmp/KMP.java
+++ mynlp/src/main/java/com/mfkcel/mynlp/util/string/kmp/KMP.java
@@ -5,4 +5,18 @@ package com.mfkcel.mynlp.util.string.kmp;
  * @date 2019/11/8 18:51
  */
 public class KMP {
+    public static int[] getNext(String str) {
+        int[] next = new int[str.length()];
+
+        for(int i = 0, j = i + 1; i < j && j < str.length();) {
+            if(str.charAt(i) == str.charAt(j)) {
+                i++;
+                next[j] = next[i] + 1;
+            } else {
+                j++;
+            }
+        }
+
+        return next;
+    }
 }
